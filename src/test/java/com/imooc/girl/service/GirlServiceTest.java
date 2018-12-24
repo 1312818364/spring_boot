98f8babef5ee446657a0203567bc57491b830099
杨锋 <yangfeng@MyMacBook-Pro.local>
2018-12-24 17:43:46 +0800

    commit

--- src/test/java/com/imooc/girl/service/GirlServiceTest.java
+++ src/test/java/com/imooc/girl/service/GirlServiceTest.java
@@ -1,7 +1,25 @@
 package com.imooc.girl.service;
 
-import static org.junit.Assert.*;
+import com.imooc.girl.domain.Girl;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class GirlServiceTest {
+
+    @Autowired
+    GirlService girlService;
+
+    @Test
+    public void findOne(){
+        Girl girl = girlService.findOne(5);
+        Assert.assertEquals(new Integer(20), girl.getAge());
+    }
 
 }
